461df3c47eb746c7f6e8e396c1892ed085817eb8
Juan David <juandavid@192.168.1.8>
2025-08-21 15:41:56 -0500

    Personalizar sidebar: logo Tempiu con ancho 98% y eliminar panel de usuario

--- src/example/yii2-views/layouts/sidebar.php
+++ src/example/yii2-views/layouts/sidebar.php
@@ -11,22 +11,11 @@ use juancho44\adminlte3\widgets\Menu;
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= Url::home() ?>" class="brand-link">
-        <img src="<?=  Url::to('@web/images/', true) ?>/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="<?= Url::to('@web/images/', true) ?>/logo.png" alt="Tempiu Logo"  style="width: 98%;">
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="<?=  Url::to('@web/images/', true) ?>profiledefault.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div>
-
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <?= Menu::widget([
